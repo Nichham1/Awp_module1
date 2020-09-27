132df14b4ce878c20f07809bff85b6d8c3153cf2
TECHN0ID <nichham2@hotmail.com>
2020-09-27 16:58:52 -0500

    Added html skeleton

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-h
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" contenet="widht=device-width, initial-scale=1.0">
+        <title>Hello World</title>
+    </head>
+
+    <body>
+
+    
+
+    </body>
+
+</html>
